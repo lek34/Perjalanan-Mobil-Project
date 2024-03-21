b3298e0f98ce3a7aeca205b1a09f279f72f9c4c8
RainJonathan <reinjonathannn@gmail.com>
2024-03-21 14:32:38 +0700

    gas

--- resources/views/admin/master/armada/create.blade.php
+++ resources/views/admin/master/armada/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.temp')
-@section('title', 'Barang')
+@section('title', 'Armada')
 
 @section('content')
     <!--begin::Toolbar-->
@@ -11,7 +11,7 @@
                 data-kt-swapper-parent="{default: '#kt_content_container', 'lg': '#kt_toolbar_container'}"
                 class="page-title d-flex align-items-center flex-wrap me-3 mb-5 mb-lg-0">
                 <!--begin::Title-->
-                <h1 class="d-flex text-dark fw-bolder fs-3 align-items-center my-1">Form Barang</h1>
+                <h1 class="d-flex text-dark fw-bolder fs-3 align-items-center my-1">Form Armada</h1>
                 <!--end::Title-->
                 <!--begin::Separator-->
                 <span class="h-20px border-gray-300 border-start mx-4"></span>
@@ -35,7 +35,7 @@
                 <div class="card-header align-items-center py-5 gap-2 gap-md-5">
                     <!--begin::Card title-->
                     <div class="card-title">
-                        <h4>Add Barang</h4>
+                        <h4>Add Armada</h4>
                     </div>
                     <!--end::Card title-->
                 </div>
@@ -53,14 +53,14 @@
                                 <div class="col-md-8">
                                     <!-- Input 1 -->
                                     <x-forms.input id="namapemilik" label="Nama Pemilik" :required="'required'" :name="'namapemilik'"
-                                        :type="'text'" :placeholder="'Masukkan Nama Pemilik...'" :func="''" :isiFunc="''"
+                                        :type="'text'" :placeholder="'Masukkan nama pemilik...'" :func="''" :isiFunc="''"
                                         :value="''" />
                                     <x-forms.input-error name="namapemilik" />
                                 </div>
                                 <div class="col-md-4">
                                     <!-- Input 3 -->
-                                    <x-forms.input id="nopol" label="Nomor Polisi" :required="'required'" :name="'nopol'"
-                                        :type="'text'" :placeholder="'Masukkan Nomor Polisi...'" :func="''" :isiFunc="''"
+                                    <x-forms.input id="namapemilik" label="Nomor Polisi" :required="'required'" :name="'nopol'"
+                                        :type="'text'" :placeholder="'Masukkan nama pemilik...'" :func="''" :isiFunc="''"
                                         :value="''" />
                                     <x-forms.input-error name="nopol" />
                                 </div>
@@ -70,22 +70,22 @@
 
                                 <div class="col-md-4">
                                     <!-- Input 4 -->
-                                    <x-forms.input id="nomormesin" label="Nomor Mesin" :required="'required'" :name="'nomormesin'"
-                                        :type="'text'" :placeholder="'Masukkan Nomor Mesin...'" :func="''" :isiFunc="''"
+                                    <x-forms.input id="nama" label="Nomor Mesin" :required="'required'" :name="'nomesin'"
+                                        :type="'text'" :placeholder="'Masukkan nama...'" :func="''" :isiFunc="''"
                                         :value="''" />
-                                    <x-forms.input-error name="nomormesin" />
+                                    <x-forms.input-error name="nomesin" />
                                 </div>
                                 <div class="col-md-4">
                                     <!-- Input 5 -->
-                                    <x-forms.input id="nomorrangka" label="Nomor Rangka" :required="'required'" :name="'nomorrangka'"
-                                        :type="'text'" :placeholder="'Masukkan Nomor Rangka...'" :func="''" :isiFunc="''"
+                                    <x-forms.input id="nama" label="Nomor Rangka" :required="'required'" :name="'norangka'"
+                                        :type="'text'" :placeholder="'Masukkan nama...'" :func="''" :isiFunc="''"
                                         :value="''" />
-                                    <x-forms.input-error name="nomorrangka" />
+                                    <x-forms.input-error name="norangka" />
                                 </div>
                                 <div class="col-md-4">
                                     <!-- Input 7 -->
-                                    <x-forms.input id="bbm" label="BBM" :required="'required'" :name="'bbm'"
-                                        :type="'text'" :placeholder="'Masukkan BBM...'" :func="''" :isiFunc="''"
+                                    <x-forms.input id="nama" label="BBM" :required="'required'" :name="'bbm'"
+                                        :type="'text'" :placeholder="'Masukkan nama...'" :func="''" :isiFunc="''"
                                         :value="''" />
                                     <x-forms.input-error name="bbm" />
                                 </div>
@@ -95,9 +95,9 @@
                                 <div class="col-md-4">
                                     <label class="fs-6 fw-bold form-label mt-3">Categories</label>
                                     <select class="form-select mb-2" data-control="select2" data-hide-search="true"
-                                        data-placeholder="Select an option">
-                                        <option selected="selected"></option>
-                                        <option value="MITSUBISHI" >MITSUBISHI</option>
+                                        data-placeholder="Select an option" name="merk">
+                                        <option></option>
+                                        <option value="MITSUBISHI" selected="selected">MITSUBISHI</option>
                                         <option value="SUZUKI">SUZUKI</option>
                                         <option value="ISUZU">ISUZU</option>
                                         <option value="TOYOTA">TOYOTA</option>
@@ -114,17 +114,17 @@
 
                                 <div class="col-md-4">
                                     <!-- Input 7 -->
-                                    <x-forms.input id="tahunproduksi" label="Tahun Produksi" :required="'required'"
-                                        :name="'tahunproduksi'" :type="'text'" :placeholder="'Masukkan Tahun Produksi...'" :func="''"
+                                    <x-forms.input id="nama" label="Tahun Produksi" :required="'required'"
+                                        :name="'tahunproduksi'" :type="'text'" :placeholder="'Masukkan nama...'" :func="''"
                                         :isiFunc="''" :value="''" />
                                     <x-forms.input-error name="tahunproduksi" />
                                 </div>
                                 <div class="col-md-4">
                                     <!-- Input 8 -->
-                                    <x-forms.input id="golongan" label="Golongan" :required="'required'" :name="'golongan'"
-                                        :type="'text'" :placeholder="'Masukkan Golongan...'" :func="''" :isiFunc="''"
+                                    <x-forms.input id="nama" label="Golongan" :required="'required'" :name="'gol'"
+                                        :type="'text'" :placeholder="'Masukkan nama...'" :func="''" :isiFunc="''"
                                         :value="''" />
-                                    <x-forms.input-error name="golongan" />
+                                    <x-forms.input-error name="gol" />
                                 </div>
                             </div>
                             <div class="row">
@@ -137,25 +137,25 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <!-- Input 6 -->
-                                    <x-forms.input id="karoseri" label="Karoseri" :required="'required'" :name="'karoseri'"
-                                        :type="'text'" :placeholder="'Masukkan Jenis Karoseri...'" :func="''" :isiFunc="''"
+                                    <x-forms.input id="namapemilik" label="Karoseri" :required="'required'" :name="'karoseri'"
+                                        :type="'text'" :placeholder="'Masukkan nama pemilik...'" :func="''" :isiFunc="''"
                                         :value="''" />
                                     <x-forms.input-error name="karoseri" />
                                 </div>
 
                                 <div class="col-md-4">
                                     <!-- Input 8 -->
-                                    <x-forms.input id="inventaris" label="Inventaris" :required="'required'" :name="'inventaris'"
-                                        :type="'text'" :placeholder="'Masukkan Lokasi Inventaris...'" :func="''" :isiFunc="''"
+                                    <x-forms.input id="nama" label="Inventaris" :required="'required'" :name="'inv'"
+                                        :type="'text'" :placeholder="'Masukkan nama...'" :func="''" :isiFunc="''"
                                         :value="''" />
-                                    <x-forms.input-error name="inventaris" />
+                                    <x-forms.input-error name="inv" />
                                 </div>
                                 <div class="col-md-4">
                                     <!-- Input 8 -->
-                                    <x-forms.input id="operasional" label="Operasional" :required="'required'" :name="'operasional'"
-                                        :type="'text'" :placeholder="'Masukkan Jenis Operasional...'" :func="''" :isiFunc="''"
+                                    <x-forms.input id="nama" label="Operasional" :required="'required'" :name="'ops'"
+                                        :type="'text'" :placeholder="'Masukkan nama...'" :func="''" :isiFunc="''"
                                         :value="''" />
-                                    <x-forms.input-error name="operasional" />
+                                    <x-forms.input-error name="ops" />
 
                                 </div>
                                 <div class="row">
@@ -165,29 +165,29 @@
                                     <div class="col-md-4">
                                         <label class="fs-6 fw-bold form-label mt-3">Mulai KIR</label>
                                         <div class="input-group date">
-                                            <input type="date" class="form-control" placeholder="Select date" name="mulaikir">
+                                            <input type="date" class="form-control" placeholder="Select date" name="lastkir">
                                         </div>
                                     </div>
                                     <div class="col-md-4">
-                                        <label class="fs-6 fw-bold form-label mt-3">Akhir KIR</label>
+                                        <label class="fs-6 fw-bold form-label mt-3">AKHIR KIR</label>
                                         <div class="input-group date">
                                             
-                                            <input type="date" class="form-control" placeholder="Select date" name="akhirkir">
+                                            <input type="date" class="form-control" placeholder="Select date" name="futurekir">
                                         </div>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-4">
-                                        <label class="fs-6 fw-bold form-label mt-3">Mulai STNK</label>
+                                        <label class="fs-6 fw-bold form-label mt-3">MULAI STNK</label>
                                         <div class="input-group date">
-                                            <input type="date" class="form-control" placeholder="Select date" name="mulaistnk">
+                                            <input type="date" class="form-control" placeholder="Select date" name="laststnk">
                                         </div>
                                     </div>
                                     
                                     <div class="col-md-4">
-                                        <label class="fs-6 fw-bold form-label mt-3">Akhir STNK</label>
+                                        <label class="fs-6 fw-bold form-label mt-3">AKHIR STNK</label>
                                         <div class="input-group date">
-                                            <input type="date" class="form-control" placeholder="Select date" name="mulaistnk">
+                                            <input type="date" class="form-control" placeholder="Select date" name="futurestnk">
                                         </div>
                                     </div>
                                 </div>
